4e503ba601d27b5a787d41cb7a96d64007c59243
Jordan Running <jordan@jordanrunning.com>
2011-03-27 17:33:56 -0500

    Creating, loading, and editing gists (in a limited way) now works

--- gister.js
+++ gister.js
@@ -14,33 +14,43 @@ Gister.prototype.CREATE_URI = 'https://gist.github.com/gists';
 // create a new gist with one file
 Gister.prototype.createOne = function createOne(filename, data, options, callback) {
   var options   = options || {}
-    , xhr       = this.getXHR(callback)
     , postData  = { login : this.username
                   , token : this.apiToken
                   , 'file_ext[gistfile1]'       : '.json'
                   , 'file_name[gistfile1]'      : filename
                   , 'file_contents[gistfile1]'  : data
                   }
-    , formData  = new FormData()
-    , paramName
   ;
 
   if(options.isPrivate) {
     postData.action_button = 'private';
   }
 
-  for(paramName in postData) {
-    if(postData.hasOwnProperty(paramName)) {
-      formData.append(paramName, postData[paramName]);
-    }
-  }
-
-  xhr.open('POST', this.CREATE_URI, true);
-  xhr.send(formData);
+  this.genericPost(this.CREATE_URI, postData, callback);
 };
 
+
+// overwrite contents of one file in a gist
+// (WARNING: Only works with one-file gists. WILL nuke additional files.)
+Gister.prototype.writeOne = function write(repoId, filename, data, callback) {
+  var filenameSplit = filename.split('.')
+    , fileExt       = filenameSplit[filenameSplit.length - 1] // yes this is naive but good enough
+    , postData      = { _method : 'put'
+                    , login   : this.username
+                    , token   : this.apiToken
+                    }
+  ;
+
+  postData['file_name[' + filename + ']']     = filename;
+  postData['file_contents[' + filename + ']'] = data;
+  postData['file_ext[' + filename + ']']      = fileExt;
+
+  this.genericPost(this.CREATE_URI + '/' + repoId, postData, callback);
+};
+
+
 // get the JSON content of a gist as an object
-Gister.prototype.getJSON = function(repoId, callback) {
+Gister.prototype.getJSON = function getJSON(repoId, callback) {
   this.get(repoId, jsonifyCallback(callback));
 };
 
@@ -81,6 +91,31 @@ Gister.prototype.genericGet = function genericGet(uri, callback) {
   var xhr = this.getXHR(callback);
   xhr.open('GET', uri, true);
   xhr.send();
+};
+
+// a generified Ajax POST--takes either a string or an object as postData
+Gister.prototype.genericPost = function genericPost(uri, postData, callback) {
+    var xhr = this.getXHR(callback)
+    , formData
+  ;
+
+
+  if(new String(postData) == postData) {
+    formData = postData;
+  } else {
+    var formData = new FormData()
+      , paramName
+    ;
+
+    for(paramName in postData) {
+      if(postData.hasOwnProperty(paramName)) {
+        formData.append(paramName, postData[paramName]);
+      }
+    }
+  }
+
+  xhr.open('POST', uri, true);
+  xhr.send(formData);
 };
 
 Gister.prototype.xhrResponseHandler = function xhrResponseHandler(xhr, callback) {
